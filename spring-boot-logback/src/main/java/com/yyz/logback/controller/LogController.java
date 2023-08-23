48ec36ac5f8aa908f48b5c3e2608df83cca9ac28
yangyezhuang <365939114@qq.com>
2023-08-23 18:14:04 +0800

    集成 logback

--- spring-boot-logback/src/main/java/com/yyz/logback/controller/LogController.java
+++ spring-boot-logback/src/main/java/com/yyz/logback/controller/LogController.java
@@ -1,7 +1,31 @@
-package com.yyz.logback;/**
+package com.yyz.logback.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
  * controller
  *
  * @author YangYezhuang
  * @date 2023/8/23 16:07
- */public class controller {
+ */
+@RestController
+public class LogController {
+    private static final Logger logger = LoggerFactory.getLogger(LogController.class);
+
+    @GetMapping("/test")
+    public void test() {
+        String name = "tom";
+        // 日志级别从低到高: TRACE < DEBUG < INFO < WARN < ERROR < FATAL
+        logger.trace("================ trace =================");
+        logger.debug("================ debug =================");
+        logger.info("================ info =================");
+        logger.warn("================ warn =================");
+        logger.error("================ error =================");
+
+        logger.info("测试用户：{}", name);
+    }
+
 }
